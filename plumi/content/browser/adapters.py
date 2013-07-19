726def805b460a35fd69da66b29f999345b4deea
Simone Orsi <simahawk@gmail.com>
2013-07-19 10:16:24 +0200

    add new colletion listing adapter

--- plumi/content/browser/adapters.py
+++ plumi/content/browser/adapters.py
@@ -2,15 +2,36 @@
 
 # ported from PlumiSkin 0.2.x trunk - 17/7/09
 from zope.interface import implements
+from zope.interface import Interface
 from zope.component import adapts
+
+from Products.CMFCore.utils import getToolByName
 from Products.ATContentTypes.interface.topic import IATTopic
 from Products.ATContentTypes.interface.folder import IATFolder
+
+try:
+    from plone.app.collection.interfaces import ICollection
+except ImportError:
+    class ICollection(Interface):
+        """ dummy klass in case we do not have p.a.collection
+        """
+
 from interfaces import IVideosProvider
-from Products.CMFCore.utils import getToolByName
 
 
 class TopicVideosProvider(object):
     adapts(IATTopic)
+    implements(IVideosProvider)
+
+    def __init__(self, context):
+        """It would be much better not to count on the acquisition and
+        old python script, and recode this part.
+        """
+        self.videos = context.queryCatalog()
+
+
+class CollectionVideosProvider(object):
+    adapts(ICollection)
     implements(IVideosProvider)
 
     def __init__(self, context):
@@ -30,7 +51,7 @@ class OrderedFolderVideosProvider(object):
         """
         if context.portal_type == 'Plumi Video Folder':
             path = "/".join(context.getPhysicalPath())
-            content_filter = dict()
+            content_filter = {}
             content_filter['path'] = path
             content_filter['portal_type']='PlumiVideo'
             content_filter['sort_on'] = 'effective'
